e316cf4d1554038854c3315e502c67603ab1dec6
kadekuru <shokaku0505@gmail.com>
2022-12-19 23:57:57 +0900

    バグ修正

--- src/App.js
+++ src/App.js
@@ -1,17 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
-import ApiFetch from './components/ApiFetch';
+import ApiFetch from "./components/ApiFetch";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        
         <ApiFetch />
-        
+
         <img src={logo} className="App-logo" alt="logo" />
-        
+
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -26,7 +25,6 @@ function App() {
       </header>
     </div>
   );
-
-
+}
 
 export default App;
